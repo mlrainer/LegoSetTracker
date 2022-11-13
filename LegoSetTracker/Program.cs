f0169e22e4ef365d0ee2022cd583aed8240630e9
mlrainer <112909069+mlrainer@users.noreply.github.com>
2022-11-13 15:29:36 -0500

    created project and first lines of code

--- LegoSetTracker/Program.cs
+++ LegoSetTracker/Program.cs
@@ -1,3 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
 
+namespace LegoSets
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Lego Tracker");
+            Console.WriteLine("How many Lego Sets Would You like to Add");
